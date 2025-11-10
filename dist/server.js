33b1dee10a1804f1bf1600b0a576091effb945c1
sdp-maker <19179977082@163.com>
2025-11-10 23:19:06 +0800

    修复bug

--- dist/server.js
+++ dist/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 // 支持从环境变量读取端口，兼容不同的部署平台
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 // 静态文件服务
 app.use(express.static(__dirname));
